ea9869f2fc81a3c9c997260f99437496fc48ac1e
Isabella Haladjian <isabellahaladjian@uls.net>
2022-01-05 14:54:22 -0500

    worked on turntotheta code

--- Swerve-v5/src/main/java/frc/robot/commands/TurnToTheta.java
+++ Swerve-v5/src/main/java/frc/robot/commands/TurnToTheta.java
@@ -1,5 +1,26 @@
 package frc.robot.commands;
 
-public class TurnToTheta {
-    //TODO: Write this class
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveDrivetrain;
+
+public class TurnToTheta extends CommandBase {
+
+    private double m_desiredAngle;
+    private SwerveDrivetrain m_drivetrain;
+    private XboxController m_controller;
+
+    public TurnToTheta(SwerveDrivetrain drivetrain, XboxController controller, double angle) {
+        m_desiredAngle = angle;
+        m_drivetrain = drivetrain;
+        m_controller = controller;
+
+        addRequirements(m_drivetrain);
+    }
+
+    @Override 
+    public void execute() {
+        
+    }
+
 }
